369bbdedaaf54090afe57269f75d4a37e34d43d8
kalucky0 <kalucki23@gmail.com>
2020-11-13 20:37:58 +0100

    Add first half of advancements editor UI

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -1,16 +1,17 @@
 let mcBlocks = [];
 
 function editRecipes() {
+    buttons.html(`<button class="btn btn-sm btn-outline-secondary" onclick="openJAR()">Open Minecraft JAR</button>`);
     const mcBlocksPicker = $("#recipe-mc-blocks").first();
     if (mcBlocks.length == 0) {
         fetch("./src/blocks.json").then(e => e.json()).then(e => {
             mcBlocks = e;
             for (let i in e)
-                mcBlocksPicker.append(`<div class="col-md-1 m-0 p-1"><img src="${e[i].texture}"></div>`);
+                mcBlocksPicker.append(`<div class="col-md-1 m-0 p-1"><img src="${e[i].texture}" draggable="true"></div>`);
         })
     } else {
         for (let i in mcBlocks)
-            mcBlocksPicker.append(`<div class="col-md-1 m-0 p-1"><img src="${mcBlocks[i].texture}"></div>`);
+            mcBlocksPicker.append(`<div class="col-md-1 m-0 p-1"><img src="${mcBlocks[i].texture}" draggable="true"></div>`);
     }
 
     $('#myTab a').on('click', function (e) {
@@ -28,6 +29,6 @@ function editRecipes() {
             return a.id.includes($(e.target).val()) || a.name.includes($(e.target).val());
         });
         for (let i in temp)
-            mcBlocksPicker.append(`<div class="col-md-1 m-0 p-1"><img src="${temp[i].texture}"></div>`);
+            mcBlocksPicker.append(`<div class="col-md-1 m-0 p-1"><img src="${temp[i].texture}" draggable="true"></div>`);
     });
 }
